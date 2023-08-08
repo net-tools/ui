d15c6ae21148b08d86bfe7a9f7055a53fb009289
nettools.ovh <dev@nettools.ovh>
2023-08-08 15:25:26 +0200

    moving files to src dir, samples

---
+++ samples/ui.js
@@ -0,0 +1,48 @@
+var sample = {
+
+    // react to color change
+    colorChange : function(sel)
+    {
+        var style = document.getElementById('colorStyle');
+        style.href = '../src/ui/ui.' + sel.value + '-theme.css';
+    },
+    
+    
+    // autoload : write color selection
+    writeColorSelection : function()
+    {
+        var sel = document.getElementById('colorSelect');
+        sel.options[0] = new Option('yellow');
+        sel.options[1] = new Option('blue');
+        sel.options[2] = new Option('dark');
+    },
+    
+    
+    // set theme
+    darkTheme : function(sel)
+    {
+        if ( sel.value == '1' )
+        {
+            document.body.style.backgroundColor = '#040e23';
+            document.body.style.color = 'white';
+        }
+        else
+        {
+            document.body.style.backgroundColor = 'white';
+            document.body.style.color = 'black';
+        }
+    },
+    
+
+    // output
+    output: function(lib)
+    {
+        document.getElementById('output').innerHTML = lib;
+    }
+    
+};
+
+
+
+// autoload
+nettools.jscore.registerOnLoadCallback(sample.writeColorSelection);
